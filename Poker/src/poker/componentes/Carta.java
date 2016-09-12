1d3330ec31d01708bfbc99d346cc2d9d9399438c
Cl3jh4n <hanjin.13@gmail.com>
2016-09-11 23:37:24 -0400

    só falta criar o metodo de comparar cartas

--- Poker/src/poker/componentes/Carta.java
+++ Poker/src/poker/componentes/Carta.java
@@ -9,23 +9,23 @@ package poker.componentes;
 public class Carta {
 
     private int valor, naipe;
-    private char[] VALORES;
+    private String[] VALORES;
     private String[] NAIPES;
 
     private Carta() {
-        VALORES = new char[12];
-        VALORES[0] = '2';
-        VALORES[1] = '3';
-        VALORES[2] = '4';
-        VALORES[3] = '5';
-        VALORES[4] = '6';
-        VALORES[5] = '7';
-        VALORES[6] = '8';
-        VALORES[7] = '9';
-        VALORES[8] = 'J';
-        VALORES[9] = 'Q';
-        VALORES[10] = 'K';
-        VALORES[11] = 'A';
+        VALORES = new String[12];
+        VALORES[0] = "2";
+        VALORES[1] = "3";
+        VALORES[2] = "4";
+        VALORES[3] = "5";
+        VALORES[4] = "6";
+        VALORES[5] = "7";
+        VALORES[6] = "8";
+        VALORES[7] = "9";
+        VALORES[8] = "J";
+        VALORES[9] = "Q";
+        VALORES[10] = "K";
+        VALORES[11] = "A";
 
         NAIPES = new String[4];
         NAIPES[0] = "Espadas";
@@ -35,27 +35,30 @@ public class Carta {
 
     }
 
-    public Carta(int valor, int naipe) { //na vdd é void segundo o uml
-        this();
+    public void Carta(int valor, int naipe) {
         this.valor = valor;
         this.naipe = naipe;
     }
 
-    public Carta(char valor, String naipe) {
-        //definir ainda
+    public Carta(String valor, String naipe) {
+        this();
+        this.Carta(Integer.parseInt(valor), Integer.parseInt(naipe));
     }
 
-    public int getNaipe(int naipe) {
+    public int getNaipe() {
         return this.naipe;
     }
 
-    public int getValor(int valor) {
+    public int getValor() {
         return this.valor;
     }
 
-    public String toString(int valor, int naipe) {
-
-        return String.valueOf(VALORES[this.valor]) + " de " + String.valueOf(NAIPES[this.naipe]);
-
+    public String toString(String aviso) {
+        if ("".equals(aviso)) {
+            aviso = VALORES[this.valor] + " de " + NAIPES[this.naipe];
+        }
+        return aviso;
     }
+    
+    //só falta a comparação
 }
