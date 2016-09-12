6920a7c6e7e82ba6c6c5182ee22c17cf3639bba7
Cl3jh4n <hanjin.13@gmail.com>
2016-09-12 13:26:33 -0400

    Classe a ser completada ps:Desenvolver a parte de criação do Baralho...

--- Poker/src/poker/componentes/Baralho.java
+++ Poker/src/poker/componentes/Baralho.java
@@ -1,31 +1,51 @@
 /*
     Classe com metodos do baralho, contendo 48 cartas distintas
-*/
+ */
 package poker.componentes;
 
-import static java.lang.Math.random;
+import java.util.Random;
 
 public class Baralho {
-    
-    /*metodo embaralhar usando random 
-    para criar a carta já embaralhada.*/
-    public void embaralhar(){
-        
-    }
-    
-    /*criar cartas utilizando embaralhar 
-    e o construtor de cartas e ja ir 
-    passando as cartas para as respectivas mãos*/
-    public Carta getCarta(int pos){
-        
-    return
-    }
-   
-    @Override //imprime o Baralho
-    public String toString(){
-        for(String l : embaralhar()){
-            System.out.println(l);
+
+    private Dado dado;
+    private int i;
+
+    public Baralho() {
+        dado = new Dado();
+        for (i = 0; i < 49;) {
+            getCarta();
         }
     }
-    
+
+    /*metodo embaralhar usando random 
+    para criar a carta já embaralhada.*/
+    public String embaralhar(int Limite) {
+        Random gerador = new Random(Limite);
+        return String.valueOf(gerador.nextInt());
+    }
+
+    /*criar cartas utilizando embaralhar 
+    e o construtor de cartas e ir adicionando ao vetor baralho*/
+    public int getCarta() {
+        Carta carta = new Carta(embaralhar(12), embaralhar(4));
+        if (!(carta.testaCartaDiferente(carta)))  { //adiciona carta ao baralho
+            dado.setTamanhoRealBaralho(dado.getTamanhoRealBaralho() + 1);
+            if (dado.getBaralho(this.i) != null) {
+                dado.setBaralho(carta, dado.getTamanhoRealBaralho());
+                System.out.println("entrou");
+            }
+        } else {
+            this.i++;
+        }
+        return this.i;
+    }
+
+    @Override //imprime o Baralho //arrumar isto
+    public String toString() {
+        String Saida = "";
+        for (Object l : dado.getBaralho(l)) {
+            Saida += "/n" + (l);
+        }
+        return Saida;
+    }
 }
