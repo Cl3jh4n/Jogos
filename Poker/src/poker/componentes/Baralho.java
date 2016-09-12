fc71a8caa3a064347c3f06f153e7f97cc959ee54
Cl3jh4n <hanjin.13@gmail.com>
2016-09-11 23:52:24 -0400

    Estrutura comentada e a ser implementada

--- Poker/src/poker/componentes/Baralho.java
+++ Poker/src/poker/componentes/Baralho.java
@@ -1,8 +1,31 @@
 /*
-    Classe com metodos do baralho, contendo 52 cartas distintas
+    Classe com metodos do baralho, contendo 48 cartas distintas
 */
 package poker.componentes;
 
+import static java.lang.Math.random;
+
 public class Baralho {
+    
+    /*metodo embaralhar usando random 
+    para criar a carta já embaralhada.*/
+    public void embaralhar(){
+        
+    }
+    
+    /*criar cartas utilizando embaralhar 
+    e o construtor de cartas e ja ir 
+    passando as cartas para as respectivas mãos*/
+    public Carta getCarta(int pos){
+        
+    return
+    }
+   
+    @Override //imprime o Baralho
+    public String toString(){
+        for(String l : embaralhar()){
+            System.out.println(l);
+        }
+    }
     
 }
