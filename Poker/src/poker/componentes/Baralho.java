60a98dfc9ee971be8817b0dcf8b88a65c2ac1bf8
Cl3jh4n <hanjin.13@gmail.com>
2016-09-13 18:21:53 -0400

    Bug do vetor estourado resolvido

--- Poker/src/poker/componentes/Baralho.java
+++ Poker/src/poker/componentes/Baralho.java
@@ -1,5 +1,10 @@
 /*
-    Classe com metodos do baralho, contendo 48 cartas distintas
+    CLasse baralho:
+        *Objeto Dado;
+    Metodos
+        *embaralhar: gera um numero aleatorio para criação da carta
+        *getCarta: cria cartas e adiciona ao objeto dado
+        *toString imprime todo o vetor de Baralho
  */
 package poker.componentes;
 
@@ -9,36 +14,35 @@ public class Baralho {
     private int i;
 
     public Baralho() {
-        dado = new Dado();
+        dado = new Dado(1);
         for (i = 0; i < 48;) {
-            getCarta();
+            this.getCarta();
         }
     }
 
     /*metodo embaralhar usando random 
     para criar a carta já embaralhada.*/
-    public String embaralhar(int Limite) {
+    public int embaralhar(int Limite) {
         int gerador;
         do {
             gerador = (int) (Math.random() * Limite);
             //System.out.println(gerador);
-
         } while (!(gerador >= 0 && gerador <= Limite));
         //System.out.println("Passou" + gerador);
-        return String.valueOf(gerador);
+        return gerador;
 
     }
 
     /*criar cartas utilizando embaralhar 
     e o construtor de cartas e ir adicionando ao vetor baralho*/
     public int getCarta() {
-        Carta carta = new Carta(embaralhar(12), embaralhar(4));
-        //System.out.println("Passou" + carta.toString(""));
-        if (carta.testaCartaDiferente(carta)) { //adiciona carta ao baral
-            dado.setBaralho(carta, dado.getTamanhoRealBaralho()+1);
-            System.out.println("entrou");
+        Carta carta = new Carta(embaralhar(11), embaralhar(3));
+        //System.out.println("Passou" + carta.toString());
+        if (dado.testaCartaDiferente(carta)) { //adiciona carta ao baral
+            dado.setBaralho(carta, dado.getTamanhoRealBaralho());
+            dado.setTamanhoRealBaralho(dado.getTamanhoRealBaralho() + 1);
+            //System.out.println("passou no teste");
             this.i++;
-        } else {
         }
         return this.i;
     }
@@ -46,9 +50,9 @@ public class Baralho {
     @Override //imprime o Baralho //arrumar isto
     public String toString() {
         String Saida = "";
-        for (i = dado.getTamanhoRealBaralho(); i <= 0 ; i--) {
+        for (i = dado.getTamanhoRealBaralho(); i <= 0; i--) {
             Carta mostra = dado.getBaralho(i);
-            Saida +=(mostra.toString());        //erro a ser descoberto
+            Saida += (mostra.toString() + "\n");        //erro a ser descoberto
         }
         return Saida;
     }
