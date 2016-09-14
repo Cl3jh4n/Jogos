24cb6cb17d34dd58c01729b835ce2dc319c85979
Cl3jh4n <hanjin.13@gmail.com>
2016-09-13 20:03:10 -0400

    Construção da classe mão

--- Poker/src/poker/componentes/Mao.java
+++ Poker/src/poker/componentes/Mao.java
@@ -1,9 +1,28 @@
 /*
-    Construtor do Objeto mão que possui exatamente oito cartas
+    Construtor da mao
+    Metodos
+        *addCarta pega 8 cartas do baralho
+        *toString imprime as cartas do baralho
 */
 package poker.componentes;
 
 
 public class Mao {
+    private String saida;
+    private Dado dado;
+    private int i;
+    Mao(){
+       dado = new Dado(2);
+    }
+    
+    public void addCarta(Carta carta){
+       dado.setMao(carta, i);
+       i++;
+    }
+    
+    /*@Override        
+    public String toString(){
+        return
+    }*/
 
 }
