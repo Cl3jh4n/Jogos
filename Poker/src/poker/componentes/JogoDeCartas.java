14dc1cdb60f80c692ad6b0a6448b26fdb9830292
Cl3jh4n <hanjin.13@gmail.com>
2016-09-14 13:09:31 -0400

    Construção inicial da classe jogo de Cartas

--- Poker/src/poker/componentes/JogoDeCartas.java
+++ Poker/src/poker/componentes/JogoDeCartas.java
@@ -1,9 +1,42 @@
 /*
-    Cria o Jogo de Cartas: Onde o Fluxo Ocorre
+    Cria o Jogo de Cartas:
+    Metodos
+        *darCarta: distribui as cartas para as maos
+        *play: Executa o jogo
+        Classe de regras
+        *cartaMaisAlta: determina e retonar a maior carta;
+        *maiorPar: verifica o conjunto recebido como um par e retorna o maior 
+            par;
  */
 package poker.componentes;
 
 public class JogoDeCartas {
 
-    
+    Dado dado;
+    Baralho baralho;
+    Mao mao, mao2;
+
+    public JogoDeCartas() {
+        dado = new Dado();
+        baralho = new Baralho(dado);
+        mao = new Mao(baralho, dado);
+        mao2 = new Mao(baralho, dado);
+    }
+
+    public static class Regras {
+
+        public boolean maiorPar() {
+            
+            return
+        }
+
+        public boolean maiorCarta() {
+            
+            return
+        }
+    }
+
+    public void play() {
+        JogoDeCartas poker = new JogoDeCartas();
+    }
 }
